842168aca1f469a3e7a69600d422f4f1641f110d
Mostafij Mozumdar <mostafijmf@gmail.com>
2022-04-18 21:27:29 +0600

    About page complete

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import profile from '../../images/profile/profile.png';
 
 const About = () => {
     return (
-        <div>
-            About
+        <div className='container'>
+            <h1 className='text-1 text-center my-5'>About me</h1>
+            <div className="row">
+                <div className="col-lg-4 col-md-3 col-sm-1"></div>
+                <div className="col-lg-4 col-md-6 col-sm-10 bg-1 p-4 rounded">
+                    <div>
+                        <div className='w-75 mx-auto my-5 bg-white border border-5 border-info rounded'>
+                            <img className='mw-100' src={profile} alt="" />
+                        </div>
+                        <p className='text-white fs-5'>
+                            Hi, this is <span className='text-2'>Mostafij</span>. A junior web developer. Now I'm learning web development deeply with Programming-Hero. I spend 8-10 hours regularly for learning code.  My goal is to become an expert-level full-stack web developer. Where I can help people to grow their businesses. And I believe that with a lot of practice and hard work, I can achieve my goal.
+                        </p>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-md-3 col-sm-1"></div>
+            </div>
         </div>
     );
 };
